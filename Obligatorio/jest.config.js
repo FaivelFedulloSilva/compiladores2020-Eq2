24f173e998580d2904016eb17b7c6c7ccfc18b8c
marcelodeleon <mdeleoncosentino@gmail.com>
2020-11-15 20:25:32 -0300

    Clear test files and add 80% coverage check

--- Obligatorio/jest.config.js
+++ Obligatorio/jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
 };
