2f921af75efededcbf0dd224663aba9927ce8c90
Marcelo de León <mdeleoncosentino@gmail.com>
2020-11-17 20:11:03 -0300

    Fix tests

--- Obligatorio/compiler.test.js
+++ Obligatorio/compiler.test.js
@@ -8,28 +8,28 @@ jest.mock('readline-sync');
 describe('compiler', () => {
   const log = 'testlog.log';
 
-  afterAll(() => fs.unlinkSync(log))
+  afterAll(() => fs.unlinkSync(log));
 
   test('Should throw exception if source extention is not .1s', async () => {
-    await expect(
-      compiler({ source: 'code.js', log }),
-    ).rejects.toThrow(/The provided extension.*/);
+    await expect(compiler({ source: 'code.js', log })).rejects.toThrow(
+      /The provided extension.*/,
+    );
   });
 
   test('Should output the result in the provided path', async () => {
     const outputPath = 'codeTest.js';
-    await compiler({ source: 'code.1s', output: outputPath, log })
+    await compiler({ source: 'code.1s', output: outputPath, log });
     expect(fs.existsSync(outputPath));
     fs.unlinkSync(outputPath);
-  })
+  });
 
   test('Should work if no output defined', async () => {
-    expect(await compiler({ source: 'code.1s', log }))
-      .toMatchInlineSnapshot(`
+    expect(await compiler({ source: 'code.1s', log })).toMatchInlineSnapshot(`
       "const g = function g(x, y) {
           x[3] = x[2] = x[1] = y;
           let w = 1;
           let z = new Date(Date.UTC(2020, 10, 24, 4));
+          let t = 1e+400;
       };"
     `);
   });
@@ -37,5 +37,5 @@ describe('compiler', () => {
   test('Should default to standard input if no source provided', async () => {
     await compiler({ log });
     expect(readline.promptLoop).toHaveBeenCalledTimes(1);
-  })
+  });
 });
