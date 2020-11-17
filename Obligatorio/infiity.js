453c75375bfde70f48de9bf472a1e66c6347a2e4
Diego Sarachaga <dsarachaga@gmail.com>
2020-11-17 08:45:35 -0300

    Update infiity.js

--- Obligatorio/infiity.js
+++ Obligatorio/infiity.js
@@ -5,7 +5,13 @@ const tt = acorn.tokTypes;
 
 module.exports = function(Parser) {
     return class extends Parser {
-        
+        parseLiteral(value) {
+            const node = super.parseLiteral(value);
+            if (node.raw.charCodeAt(node.raw.length - 1) == 110)
+              node.bigint = this.getNumberInput(node.start, node.end - 1);
+            return node;
+          }
+      
         readWord = function () {
             var ch = this.fullCharCodeAtPos()
             if ((ch === 85) || (ch === 117)){ //U u
@@ -14,11 +20,13 @@ module.exports = function(Parser) {
                 if (ch === 43){ //+
                     ++this.pos;
                     str = this.getNumberInput(this.pos, this.pos+3);
-                    ++this.pos;
-                    var ch = this.fullCharCodeAtPos()
-                    if ((ch === 69) || (ch === 101)){ //E e
-                        return this.finishToken(tt.num, Infinity);
+                    if (str===221){
+                        ++this.pos;
+                        var ch = this.fullCharCodeAtPos()
+                        if ((ch === 69) || (ch === 101)){ //E e
+                            return this.finishToken(tt.num, Infinity);
                     }
+                }
                 }
             }
         }
